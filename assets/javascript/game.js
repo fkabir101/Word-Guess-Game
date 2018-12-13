f7fc20485fe9b66018d914afae4c015604b382af
Fahim Kabir <fkabir101@gmail.com>
2018-12-13 17:36:08 -0500

    Initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// create wordbank
+var wordBank = [word, anotherword];
+
+
+// funtion to return a word from wordBank
+
+function getWord(){
+  return wordBank[Math.floor(Math.random()*wordBank.length)]
+}
